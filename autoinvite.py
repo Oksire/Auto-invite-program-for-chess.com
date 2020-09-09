82b06e8a65083333554913c128908e135a185230
1qazxcde32 <61962807+1qazxcde32@users.noreply.github.com>
2020-09-09 16:20:08 +1000

    Update autoinvite.py

--- autoinvite.py
+++ autoinvite.py
@@ -1,23 +1,35 @@
 import requests
+from selenium import webdriver
 import time
-from helium import *
-def invite(link,username,password,x):
-    r = requests.get('https://api.chess.com/pub/club/chess-university/members')
-    members = r.json()['all_time'][x:x+30]        
-    invitelist = []
-    for i in members:
-        invitelist.append(i['username'])
-    print(invitelist)
-    driver = start_chrome('https://www.chess.com/login')
+import json
+
+with open('C:\\path_to_json_file\\example.json') as f:
+    data = json.load(f)
+driver = webdriver.Chrome(executable_path=data['path'])
+driver.get('https://www.chess.com/login')
+time.sleep(3)
+driver.find_element_by_id('username').send_keys(data['username'])
+driver.find_element_by_id('password').send_keys(data['password'])
+driver.find_element_by_id('login').click()
+r = requests.get(data['apilink'])
+members = r.json()[data['timeframe']]
+for i in data['invitelist']:
+    driver.get(i['invitelink'])
     time.sleep(3)
-    driver.find_element_by_id('username').send_keys(username)
-    driver.find_element_by_id('password').send_keys(password)
-    driver.find_element_by_id('login').click()
-    time.sleep(3)
-    driver.get(link)
-    time.sleep(3)
-    for i in invitelist:
-        driver.find_element_by_class_name('form-input-input').send_keys(i+' ')
-        time.sleep(1)
-    time.sleep(1)
-    driver.find_element_by_id('members_invite_invite').click()
+    try:
+        x = int(driver.find_element_by_class_name('clubs-manage-members-text').text[:2])
+        invites = members[data['number']:data['number']+x]
+        invitelist = [j['username'] for j in invites]
+        for n in invitelist:
+            driver.find_element_by_class_name('form-input-input').send_keys(n+' ')
+            time.sleep(1)
+        print(invitelist)
+        print(i['clubname'],x)
+        driver.find_element_by_id('members_invite_invite').click()
+        time.sleep(2)
+    except:
+        continue
+data['number'] += 30
+a_file = open('C:\\path_to_json_file\\example.json', 'w')
+json.dump(data, a_file)
+a_file.close()
